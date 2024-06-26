01bf4b9a836bad8f4f223db4334813c621979510
Brendan Boyle <brendanboyle02@gmail.com>
2024-06-26 14:48:10 -0500

    mock user model for repository tests

--- server/src/repositories/user/user.repository.spec.ts
+++ server/src/repositories/user/user.repository.spec.ts
@@ -1,48 +1,62 @@
 import DbConfig from "../../configs/db.config";
-import UserRepository from "./user.repository";
 import { User } from "../../models/user.model";
-import { MongooseError } from "mongoose";
+import UserRepository from "./user.repository";
 
 describe("User Repository", () => {
   let userRepository: UserRepository;
-  let userData = {
+  let testUser = {
     name: "John Doe",
     email: "john@email.com",
     password: "password123",
   };
-  beforeAll(async () => {
-    await DbConfig.connect("test");
+
+  beforeEach(async () => {
+    // Mock new User.save() to return the test user
+    jest
+      .spyOn(User.prototype, "save")
+      .mockImplementationOnce(() => Promise.resolve(new User(testUser)));
+
     userRepository = new UserRepository(User);
   });
 
-  afterAll(async () => {
-    // Drop all created objects from the test database
-    await User.deleteMany({});
-    await DbConfig.disconnect();
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   test("should create a new user", async () => {
-    const result = await userRepository.create(userData);
-    expect(result.email).toEqual(userData.email);
-    expect(result.name).toEqual(userData.name);
+    const result = await userRepository.create(testUser);
+    expect(result.email).toEqual(testUser.email);
+    expect(result.name).toEqual(testUser.name);
   });
 
   test("should thhrow error if user already exists", async () => {
+    // Mock User.save() to throw duplicate key error
+    jest
+      .spyOn(User.prototype, "save")
+      .mockImplementationOnce(() =>
+        Promise.reject(new Error("E11000 duplicate key"))
+      );
+
     // Email must be unique for each user
     try {
-      const result = await userRepository.create(userData);
+      const result = await userRepository.create(testUser);
     } catch (error: any) {
       expect(error.message).toContain("E11000 duplicate key");
     }
   });
 
   test("should throw error if user data is invalid", async () => {
-    userData.name = "";
-    userData.email = "";
-    userData.password = "";
-
+    // Mock User.save() to throw validation error
+    jest
+      .spyOn(User.prototype, "save")
+      .mockImplementationOnce(() =>
+        Promise.reject(new Error("User validation failed"))
+      );
+    testUser.name = "";
+    testUser.email = "";
+    testUser.password = "";
     try {
-      const result = await userRepository.create(userData);
+      const result = await userRepository.create(testUser);
     } catch (error: any) {
       expect(error.message).toContain("User validation failed");
     }
